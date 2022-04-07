002132e5a377ffb5dab5e4ba5e58f5a5082c1b7a
Vasiliy Kalmykov <kalmykov.vk@phystech.edu>
2022-04-07 12:49:41 +0300

    initial commit

---
+++ src/graphml.hpp
@@ -0,0 +1,8 @@
+//
+// Created by kvk1920 on 4/6/22.
+//
+
+#ifndef DATAVIS_SRC_GRAPHML_HPP_
+#define DATAVIS_SRC_GRAPHML_HPP_
+
+#endif //DATAVIS_SRC_GRAPHML_HPP_
---
+++ src/svg.hpp
@@ -0,0 +1,8 @@
+//
+// Created by kvk1920 on 4/6/22.
+//
+
+#ifndef DATAVIS_SRC_SVG_HPP_
+#define DATAVIS_SRC_SVG_HPP_
+
+#endif //DATAVIS_SRC_SVG_HPP_
